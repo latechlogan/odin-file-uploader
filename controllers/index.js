c7d93237ccc551aae78293a5e0c1545253947bcc
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2026-02-24 00:32:34 -0600

    feat: implement index page with auth and data

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,18 @@
-const homepage = (req, res) => {
-  res.send(`Hello, ${req.user ? req.user.username : "World"}!`);
+import { prisma } from "../lib/prisma.js";
+
+const homepage = async (req, res) => {
+  const user = req.user || null;
+  const { search = "", type = "all" } = req.query;
+
+  if (user) {
+    const files = await prisma.file.findMany({ where: { userId: user.id } });
+    const folders = await prisma.folder.findMany({
+      where: { userId: user.id },
+    });
+    res.render("index", { user, search, type, files, folders });
+  } else {
+    res.render("index", { user, search, type, files: [], folders: [] });
+  }
 };
 
 export default { homepage };
