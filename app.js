ae5d8c0590967d1f4cb3e5c25c3875c57ba9f6a7
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2026-03-10 10:12:03 -0500

    refactor: add env PORT variable with fallback

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ import filesRouter from "./routes/files.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
